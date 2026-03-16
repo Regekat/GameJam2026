5140e1121abcdeab3afa3ac5621854a9dfe23b87
Beans Phd. <70842120+Regekat@users.noreply.github.com>
2026-03-17 02:59:06 +0800

    Commit #5.3: Grocery List

---
+++ Supermarket Racer/Assets/Scripts/GroceryList.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GroceryList : MonoBehaviour
+{
+    [SerializeField] private RawImage groceryPaper;
+    [SerializeField] private GameObject guideText;
+
+    private RectTransform groceryPaperRect;
+
+    [SerializeField] private Vector2 listShownPosition = new Vector2(335f, -160f);
+    [SerializeField] private Vector2 listHiddenPosition = new Vector2(335f, -885f);
+
+    [SerializeField] private bool listStartsShown = false;
+
+    private bool listShown;
+
+    private void Awake()
+    {
+        if (groceryPaper == null)
+        {
+            Debug.LogError("GroceryPaper RawImage is not assigned in the Inspector.");
+            return;
+        }
+
+        if (guideText == null)
+        {
+            Debug.LogError("GuideText is not assigned in the Inspector.");
+            return;
+        }
+
+        groceryPaperRect = groceryPaper.GetComponent<RectTransform>();
+
+        if (groceryPaperRect == null)
+        {
+            Debug.LogError("GroceryPaper does not have a RectTransform.");
+            return;
+        }
+
+        listShown = listStartsShown;
+        ApplyState();
+    }
+
+    private void Update()
+    {
+        if (groceryPaperRect == null) return;
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            listShown = !listShown;
+            ApplyState();
+        }
+    }
+
+    private void ApplyState()
+    {
+        groceryPaperRect.anchoredPosition = listShown ? listShownPosition : listHiddenPosition;
+        guideText.SetActive(!listShown);
+    }
+}
